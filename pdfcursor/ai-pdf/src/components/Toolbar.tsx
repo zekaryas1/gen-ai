1a873caa63f409141459d410fb54fe0f10cc0f93
Zekaryas Tadele <zekarasdinku@gmail.com>
2025-06-23 13:22:50 +0300

    - fix: meaning-full naming

--- pdfcursor/ai-pdf/src/components/Toolbar.tsx
+++ pdfcursor/ai-pdf/src/components/Toolbar.tsx
@@ -1,10 +1,10 @@
 interface ToolbarProps {
-  currentPage: number;
+  pageNumber: number;
   totalPages: number;
 }
 
 export default function Toolbar(props: ToolbarProps) {
-  const { currentPage, totalPages } = props;
+  const { pageNumber, totalPages } = props;
   return (
     <div
       className={"p-4 bg-white text-black border-y shadow-lg flex items-center"}
@@ -13,7 +13,7 @@ export default function Toolbar(props: ToolbarProps) {
       }}
     >
       <p>
-        Page {currentPage} of {totalPages}
+        Page {pageNumber} of {totalPages}
       </p>
     </div>
   );
