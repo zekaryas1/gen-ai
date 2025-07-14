a36c86bb235cdd0e7eac5a7bb71046da200a067a
Zekaryas Tadele <zekarasdinku@gmail.com>
2025-07-14 16:43:41 +0300

    - static export with pwa support

--- pdfcursor/ai-pdf/next.config.ts
+++ pdfcursor/ai-pdf/next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   /* config options here */
-    turbopack: {
-        resolveAlias: {
-            canvas: './empty-module.ts',
-        },
-    }
+  turbopack: {
+    resolveAlias: {
+      canvas: "./empty-module.ts",
+    },
+  },
 };
 
 export default nextConfig;
