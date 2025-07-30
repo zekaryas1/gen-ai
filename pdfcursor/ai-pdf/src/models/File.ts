c70f2d35308f0c00ff06ccee26870a7121b37715
Zekaryas Tadele <zekarasdinku@gmail.com>
2025-07-30 21:10:53 +0300

    - model definition for PdfStateType

--- pdfcursor/ai-pdf/src/models/File.ts
+++ pdfcursor/ai-pdf/src/models/File.ts
@@ -1,3 +1,6 @@
+import { OutlineItem } from "@/models/OutlineItem";
+import { RESIZABLE_PANEL_DESKTOP_OPTIONS } from "@/utils/constants.utils";
+
 export interface LocalStorageFile {
   title: string;
   fileName: string;
@@ -6,4 +9,12 @@ export interface LocalStorageFile {
   lastVisitedPage: number;
   lastOpenedDate: number;
   outlineState: string[];
+}
+
+export interface PdfStateType {
+  fileName: string;
+  outline: OutlineItem[];
+  lastPagePosition: number;
+  outlineState: string[];
+  panelOptions: typeof RESIZABLE_PANEL_DESKTOP_OPTIONS;
 }
