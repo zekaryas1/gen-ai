33a9e98590b1f62c9bdc1f7d59d4e3a4b8fcbe4b
Zekaryas Tadele <zekarasdinku@gmail.com>
2025-07-30 21:10:09 +0300

    - ui: fix

--- pdfcursor/ai-pdf/src/components/OutlineToolbar.tsx
+++ pdfcursor/ai-pdf/src/components/OutlineToolbar.tsx
@@ -5,18 +5,13 @@ import { useRouter } from "next/navigation";
 export default function OutlineToolbar({ fileName }: { fileName: string }) {
   const router = useRouter();
 
+  const handleBackClick = () => {
+    router.back();
+  };
+
   return (
-    <div
-      className={
-        "flex justify-between items-center border p-1.5 border-e-0 gap-4 h-12"
-      }
-    >
-      <Button
-        variant={"ghost"}
-        onClick={() => {
-          router.back();
-        }}
-      >
+    <div className={"flex items-center border p-1.5 border-e-0 gap-4 h-12"}>
+      <Button variant={"ghost"} onClick={handleBackClick}>
         <ArrowLeft />
       </Button>
       <p className={"text-sm line-clamp-1 text-ellipsis"} title={fileName}>
