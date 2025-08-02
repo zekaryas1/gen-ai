c8d047d5b844781806c65d1a452c206c3c223194
Zekaryas Tadele <zekarasdinku@gmail.com>
2025-08-02 21:36:59 +0300

    moved pdf cursor to aku-ai-pdf repo

--- pdfcursor/ai-pdf/src/components/ScrollPlaceHolder.tsx
+++
@@ -1,7 +0,0 @@
-export default function ScrollPlaceHolder() {
-  return (
-    <div className="h-svh bg-gray-200 animate-pulse flex justify-center items-center">
-      <p>Loading your page...</p>
-    </div>
-  );
-}
